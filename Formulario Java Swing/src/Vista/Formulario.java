98bcafbb7f03b11823e1659b737120b99f66663b
Mauricio Ivan Mejia Ramirez <mejiaramirez@outlook.com>
2015-01-12 17:10:03 -0600

    Declarados componentes del formulario

--- Formulario Java Swing/src/Vista/Formulario.java
+++ Formulario Java Swing/src/Vista/Formulario.java
@@ -1,10 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Vista;
+
+import java.awt.Container;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class Formulario {
     
+    // A mi me gusta declarar el frame en lugar de extender la clase y entre parentesis va el titulo de este
+    JFrame framePrincipal = new JFrame("Formulario de Datos Personales");
+
+    // Definimos el contenedor
+    Container contenedor = framePrincipal.getContentPane();
+
+    // Las etiquetas de los campos con su texto entre parentesis
+    JLabel aMaternoLabel =  new JLabel("Apellido Materno");
+    JLabel aPaternoLabel = new JLabel("Apellido Paterno");
+    JLabel checkLabel = new JLabel("Acepto ser Humano");
+    JLabel nombreLabel = new JLabel("Nombre");
+    JLabel sexoLabel = new JLabel("sexo");
+
+    // Los campos de texto
+    JTextField aMaternoField = new JTextField();
+    JTextField aPaternoField = new JTextField();
+    JTextField nombreField = new JTextField();
+
+    // El Combobox y su arreglo de opciones
+    String [] sexoOptions = {"Masculino","Femenino"};
+    JComboBox sexoCombo = new JComboBox(sexoOptions);
 }
